25bbed961d18fd576302999e238e070c2a20e7e7
FoxyWoxy12000 <140989615+FoxyWoxy12000@users.noreply.github.com>
2026-06-21 07:41:34 -0700

    Update ServerPlayerGameModeMixin.java

--- src/main/java/dev/d4vid/mods/genesis/server/mixin/event/ServerPlayerGameModeMixin.java
+++ src/main/java/dev/d4vid/mods/genesis/server/mixin/event/ServerPlayerGameModeMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -41,7 +42,9 @@ public class ServerPlayerGameModeMixin {
         }
     }
 
+    @Shadow
     public ServerPlayer player;
+
     @Inject(method = "destroyBlock", at = @At("HEAD"), cancellable = true)
     private void genisis$destroyBlock(BlockPos pos, CallbackInfoReturnable<Boolean> info) {
         if (player.gameMode.getGameModeForPlayer() == GameType.SURVIVAL) {
