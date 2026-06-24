e5efd20970dfaab76dc470cd9abe3ccb38edb6bc
David Malchin <malchin459@gmail.com>
2026-06-25 01:39:25 +0300

    Rewrite

--- src/main/kotlin/dev/d4vid/mods/genesis/server/Genesis.kt
+++ src/main/kotlin/dev/d4vid/mods/genesis/server/Genesis.kt
@@ -1,12 +1,18 @@
 package dev.d4vid.mods.genesis.server
 
-import dev.d4vid.mods.genesis.server.block.registerBlockDestroyPrevention
-import dev.d4vid.mods.genesis.server.combat.registerInCombatDetector
-import dev.d4vid.mods.genesis.server.combat.registerPvpProtection
-import dev.d4vid.mods.genesis.server.item.registerBloodlustKillTracker
-import dev.d4vid.mods.genesis.server.limit.registerInventoryLimitEnforcer
-import dev.d4vid.mods.genesis.server.recipe.registerRecipeDisabler
-import dev.d4vid.mods.genesis.server.resourcePack.registerResourcePackLoader
+import dev.d4vid.mods.genesis.server.blocks.initializeUnbreakableBlockHandler
+import dev.d4vid.mods.genesis.server.command.genesisCommand
+import dev.d4vid.mods.genesis.server.command.registerCommand
+import dev.d4vid.mods.genesis.server.config.GenesisConfig
+import dev.d4vid.mods.genesis.server.cooldowns.ItemCooldownHandler
+import dev.d4vid.mods.genesis.server.cooldowns.LungeCooldownHandler
+import dev.d4vid.mods.genesis.server.custom.item.GenesisItems
+import dev.d4vid.mods.genesis.server.items.ItemLimitHandler
+import dev.d4vid.mods.genesis.server.items.initializeDisabledItemsHandler
+import dev.d4vid.mods.genesis.server.portals.initializePortalsHandler
+import dev.d4vid.mods.genesis.server.pvp.CombatDetectionHandler
+import dev.d4vid.mods.genesis.server.pvp.CombatProtectionHandler
+import dev.d4vid.mods.genesis.server.recipes.initializeDisabledRecipeHandler
 import net.fabricmc.api.DedicatedServerModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -14,19 +20,32 @@ import org.slf4j.LoggerFactory
 @Suppress("unused")
 object Genesis : DedicatedServerModInitializer {
     const val MOD_ID = "genesis"
-    val logger: Logger = LoggerFactory.getLogger(MOD_ID)
+    private val logger: Logger = LoggerFactory.getLogger(MOD_ID)
+
+    private val config = GenesisConfig()
+    private val itemLimit = ItemLimitHandler(config)
+    private val combatDetection = CombatDetectionHandler(config)
+    private val combatProtection = CombatProtectionHandler(config)
+    private val lungeCooldown = LungeCooldownHandler(config, combatDetection)
+    private val itemCooldown = ItemCooldownHandler(config, combatDetection)
 
     override fun onInitializeServer() {
-        registerCommand()
-        registerBlockDestroyPrevention()
-        registerInCombatDetector()
-        registerResourcePackLoader()
-        registerRecipeDisabler()
-        registerInventoryLimitEnforcer()
-        registerPvpProtection()
+        config.initialize()
+        registerCommand(genesisCommand(config, combatProtection))
 
-        registerBloodlustKillTracker()
+        initializeDisabledItemsHandler(config, combatDetection)
+        itemLimit.initialize()
 
-        GenesisConfig.loadFile()
+        initializeUnbreakableBlockHandler(config)
+        initializeDisabledRecipeHandler(config)
+        initializePortalsHandler(config)
+
+        lungeCooldown.initialize()
+        itemCooldown.initialize()
+
+        combatDetection.initialize()
+        combatProtection.initialize()
+
+        GenesisItems.initialize()
     }
 }
---
+++ src/main/kotlin/dev/d4vid/mods/genesis/server/config/data/pvp/PvpConfig.kt
@@ -0,0 +1,32 @@
+package dev.d4vid.mods.genesis.server.config.data.pvp
+
+import dev.d4vid.mods.genesis.server.config.serialization.NonNegativeDurationMinutesDoubleSerializer
+import kotlinx.serialization.Serializable
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.scores.Team
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
+@Serializable
+data class PvpConfig(
+    private val protectedTeams: Set<String> = setOf("peaceful"),
+    private val spawnProtection: PvpSpawnProtectionConfig = PvpSpawnProtectionConfig(),
+    @Serializable(with = NonNegativeDurationMinutesDoubleSerializer::class)
+    val joinProtectionMinutes: Duration = 5.minutes,
+    @Serializable(with = NonNegativeDurationMinutesDoubleSerializer::class)
+    val respawnProtectionMinutes: Duration = 60.minutes,
+    val detection: PvpDetectionConfig = PvpDetectionConfig(),
+    val killPlayerOnCombatLog: Boolean = true,
+) {
+    fun isTeamProtected(team: Team): Boolean {
+        return protectedTeams.contains(team.name)
+    }
+
+    fun isSpawnProtected(player: ServerPlayer): Boolean {
+        val dx = player.x - spawnProtection.x
+        val dz = player.z - spawnProtection.z
+        val radius = spawnProtection.radius
+
+        return (dx * dx + dz * dz) <= (radius * radius)
+    }
+}
