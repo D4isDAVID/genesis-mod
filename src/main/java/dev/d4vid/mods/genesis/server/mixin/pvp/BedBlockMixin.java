66cb13a426670f6f439a9725dfb3ee56da35bdaa
David Malchin <malchin459@gmail.com>
2026-07-13 23:43:44 +0300

    Add bed explosion nerf

---
+++ src/main/java/dev/d4vid/mods/genesis/server/mixin/pvp/BedBlockMixin.java
@@ -0,0 +1,25 @@
+package dev.d4vid.mods.genesis.server.mixin.pvp;
+
+import dev.d4vid.mods.genesis.server.event.GenesisCombatEvents;
+import net.minecraft.world.level.block.BedBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@SuppressWarnings("unused")
+@Mixin(BedBlock.class)
+public class BedBlockMixin {
+    @ModifyArg(
+        method = "useWithoutItem",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/Level;explode(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/damagesource/DamageSource;Lnet/minecraft/world/level/ExplosionDamageCalculator;Lnet/minecraft/world/phys/Vec3;FZLnet/minecraft/world/level/Level$ExplosionInteraction;)V"
+        ),
+        index = 4
+    )
+    private float genesis$useWithoutItem(float radius) {
+        Float result = GenesisCombatEvents.INSTANCE.getMODIFY_BED_EXPLOSION_RADIUS().invoker().modifyBedExplosionRadius(radius);
+
+        return result == null ? radius : result;
+    }
+}
