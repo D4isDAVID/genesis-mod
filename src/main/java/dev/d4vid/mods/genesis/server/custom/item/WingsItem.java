5713c16f19e56f8be6d5f39db3664cb8d6e6cf3a
FoxyWoxy12000 <140989615+FoxyWoxy12000@users.noreply.github.com>
2026-07-29 19:01:02 -0700

    we

--- src/main/java/dev/d4vid/mods/genesis/server/custom/item/WingsItem.java
+++ src/main/java/dev/d4vid/mods/genesis/server/custom/item/WingsItem.java
@@ -5,11 +5,16 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.Unit;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.component.ItemLore;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.minecraft.world.item.equipment.EquipmentAssets;
+import net.minecraft.world.item.equipment.Equippable;
 
 import java.util.List;
 
@@ -29,6 +34,7 @@ public class WingsItem extends GenesisItem {
         //item.set(DataComponents.UNBREAKABLE, Unit.INSTANCE);
         enchant(registries, item);
         applyLore(item);
+        applyEquippable(item);
     }
 
     private void enchant(RegistryAccess registries, ItemStack item) {
@@ -44,6 +50,17 @@ public class WingsItem extends GenesisItem {
             Component.literal("WIP")
                 .withStyle(s -> s.withItalic(false).withBold(true).withColor(LORE_COLOR))
         )));
+    }
+
+    private void applyEquippable(ItemStack item) {
+        ResourceKey<EquipmentAsset> assetKey = ResourceKey.create(
+            EquipmentAssets.ROOT_ID,
+            Identifier.fromNamespaceAndPath("genesis", "wings")
+        );
+        item.set(DataComponents.EQUIPPABLE, Equippable.builder(EquipmentSlot.CHEST)
+            .setAsset(assetKey)
+            .setDamageOnHurt(false)
+            .build());
     }
     @Override
     public boolean canBeEnchanted() {
